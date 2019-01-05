65e5134136b39f9784cf4d015eb4bb208099ee9d
onlycau <990246984@qq.com>
2019-01-05 17:25:04 +0800

    Update sender.py

--- client/sender.py
+++ client/sender.py
@@ -1,11 +1,16 @@
 import subprocess
 import time
-
-from client.config import *
+ 
 import requests
-
+ 
+url = 'http://114.116.121.45:5000'
+headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36'}
+ 
 while True:
-    subprocess.run(ADSL_BASH)
-    sleep(2)
-    requests.get(SERVER_URL)
-    sleep(ADSL_CYCLE)
+    p = subprocess.Popen('pppoe-stop',shell = True)
+    time.sleep(3)
+    p = subprocess.Popen('pppoe-start',shell = True)
+    time.sleep(200)
+    r = requests.get(url,headers = headers)
+    print(r)
+    time.sleep(400)
